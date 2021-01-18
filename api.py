4dcede177ff63343c902aad2f54f44af7d39ad39
RomanJ55 <roman.jaro@web.de>
2021-01-18 02:14:20 +0100

    added a dummy board, added more socketio listeners, added rooms(for two clients, with broadcasting data to the room)

--- api.py
+++ api.py
@@ -2,8 +2,8 @@ import json
 from flask import Flask, render_template
 from flask_socketio import SocketIO, join_room, leave_room, emit, send, rooms
 from game import Game
-import eventlet
-eventlet.monkey_patch()
+# import eventlet
+# eventlet.monkey_patch()
 
 
 app = Flask(__name__)
@@ -12,70 +12,118 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*",
                     manage_session=False, max_http_buffer_size=1e8)
 
-games = {0: Game()}
+
+def make_dummy_game():
+    dummy_game = Game(1800)
+    dummy_game.reset_game()
+    dummy_game.game_running = True
+    dummy_game.timer = "not started"
+    dummy_game.turn = "not started"
+
+    return dummy_game
+
+
+games = {0: ["", "", False,  make_dummy_game()]}
 
 
 @socketio.on('join', "/game")
 def on_join(data):
+    username = data['username']
     room = data['room']
     join_room(room)
-    games[room] = Game()
-    #send(username + ' has entered the room.', room=room)
+    games[room] = [username, "", False, make_dummy_game()]
+    send(username + ' has entered the room.', room=room)
 
 
-@socketio.on('leave')
+@socketio.on('joinExisting', "/game")
+def on_joinExisting(data):
+    username = data['username']
+    room = int(data['room'])
+    if room in games.keys():
+        games[room][1] = username
+        games[room][3] = Game()
+        games[room][2] = True
+        join_room(room)
+    else:
+        emit("joinExisting", "Wrong code")
+    send(username + ' has entered the room.', room=room)
+
+
+@socketio.on('leave', "/game")
 def on_leave(data):
-    room = data['room']
+    room = data["room"]
+    username = data["username"]
+
+    game = games[room]
+    if game[0] == username:
+        game[0] = ""
+    if game[1] == username:
+        game[1] = ""
+    game[2] = False
+    game[3] = make_dummy_game()
+
+    if (game[0] == "" and game[1] == ""):
+        games.pop(room)
     leave_room(room)
-    #send(username + ' has left the room.', room=room)
+    send(username + ' has left the room.', room=room)
 
 
 @socketio.on("data", "/game")
 def on_data():
     game = None
+    room_id = 0
     room = rooms()
     if len(room) > 1:
-        game = games[room[1]]
+        for r in room:
+            if isinstance(r, int):
+                room_id = r
+        game_entry = games[room_id]
+        if game_entry[2]:
+            game = game_entry[3]
+        else:
+            game = games[0][3]
     else:
-        game = games[0]
-    socketio.sleep(0)
+        game = games[0][3]
     json_str = json.dumps(
         game, default=lambda x: x.__dict__, indent=2)
-    emit("data", json_str)
+    emit("data", json_str, room=room_id)
 
 
 @socketio.on("gameStart", "/game")
-def on_start():
-    game = None
+def on_gameStart():
     room = rooms()
-    if len(room) > 1:
-        game = games[room[1]]
-    else:
-        game = games[0]
-    game.run_game()
+    room_id = 0
+    for r in room:
+        if isinstance(r, int):
+            room_id = r
+    game_entry = games[room_id]
+    game_entry[3].run_game()
     emit("gameStart", "game started!")
 
 
 @socketio.on("gameEnd", "/game")
-def on_end():
-    game = None
+def on_gameEnd():
     room = rooms()
-    if len(room) > 1:
-        game = games[room[1]]
-    else:
-        game = games[0]
-    game.stop_game("black" if game.turn == "white" else "white")
+    room_id = 0
+    for r in room:
+        if isinstance(r, int):
+            room_id = r
+    game_entry = games[room_id]
+    game_entry[3].stop_game(
+        "black" if game_entry[3].turn == "white" else "white")
     emit("gameEnd", "game ended!")
 
 
 @socketio.on("clicked", "/game")
 def on_clicked(data):
-    game = None
     room = rooms()
-    if len(room) > 1:
-        game = games[room[1]]
-    else:
-        game = games[0]
+    room_id = 0
+    for r in room:
+        if isinstance(r, int):
+            room_id = r
+    game_entry = games[room_id]
+    game = game_entry[3]
+
     piece_color = 0
     x, y = data["x"], data["y"]
     if len(data) > 2:
@@ -85,10 +133,8 @@ def on_clicked(data):
             game.unselect_all()
             game.board[x][y].select()
         else:
-            socketio.sleep(0)
             initiate_piece_move(x, y, game)
     else:
-        socketio.sleep(0)
         initiate_piece_move(x, y, game)
     emit("clicked", "Done!")
 
